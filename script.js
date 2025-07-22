8f3bd8794458133ae9899a53b6e3336772163dbf
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2025-07-21 22:39:59 -0300

    variáveis
    array com as imgs e variável para duplicar as imgs

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+const imgCartas = [
+    'estilos/imagens/cachorro-amarelo.png',
+    'estilos/imagens/cachorro-marrom.png',
+    'estilos/imagens/cachorro-preto.png',
+    'estilos/imagens/gato-amarelo.png',
+    'estilos/imagens/gato-branco.png',
+    'estilos/imagens/gato-roxo.png'
+];
+ // Duplica as imagens para formar os pares
+const imgEmbaralhadas = [...imgCartas, ...imgCartas];
+
+ 
+});
+
